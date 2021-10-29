8a9b94aa39ff33173b5f96b1a5771d3917868592
rosbif <jack.goodall@epitech.eu>
2021-10-29 20:31:13 +0200

    fix: deleted post component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,23 @@
-import React, { useEffect } from "react"
-import { ScrollView, StyleSheet, View } from "react-native";
-import { Button, useTheme, Text } from "react-native-paper";
-import { getPostsListing } from "../api/subreddits";
-import PostCard from "../components/home/PostCard";
-import LoadingModal from "../components/LoadingModal";
-import useListing from "../hooks/useListing";
-import { useAuth } from "../providers/AuthProvider";
-import { useRuntimeInfo } from "../providers/RuntimeInfoProvider";
+import React, {useEffect} from 'react';
+import {ScrollView, StyleSheet, View} from 'react-native';
+import {Button, useTheme, Text} from 'react-native-paper';
+import {getPostsListing} from '../api/subreddits';
+import PostCard from '../components/home/PostCard';
+import LoadingModal from '../components/LoadingModal';
+import useListing from '../hooks/useListing';
+import {useAuth} from '../providers/AuthProvider';
+import {useRuntimeInfo} from '../providers/RuntimeInfoProvider';
 
 export default Home = () => {
   const theme = useTheme();
   const styles = useStyle(theme.colors);
   const rtInfo = useRuntimeInfo();
-  const { refreshAccessToken } = useAuth();
-  const { data, getPrev, getNext, loading } = useListing(getPostsListing, 'r/darkjokes', 1);
+  const {refreshAccessToken} = useAuth();
+  const {data, getPrev, getNext, loading} = useListing(
+    getPostsListing,
+    'r/darkjokes',
+    1,
+  );
 
   useEffect(() => {
     refreshAccessToken();
@@ -21,25 +25,31 @@ export default Home = () => {
 
   if (data[0]?.error) {
     return (
-      <View style={{ ...styles.root, flex: 1 }}>
+      <View style={{...styles.root, flex: 1}}>
         <Text>{data[0].error}</Text>
       </View>
-    )
+    );
   }
 
   return (
-    <ScrollView style={{ ...styles.root }}>
+    <ScrollView style={{...styles.root}}>
       <LoadingModal enabled={loading} />
-      {data.map((child, index) =>
-        <PostCard postData={child.data} key={index}></PostCard>
-      )}
-      <Button onPress={() => { getNext() }}>next</Button>
+      {data.map((child, index) => (
+        <PostCard postData={child.data} key={index} />
+      ))}
+      <Button
+        onPress={() => {
+          getNext();
+        }}>
+        next
+      </Button>
     </ScrollView>
-  )
-}
+  );
+};
 
-const useStyle = (colors) => StyleSheet.create({
-  root: {
-    backgroundColor: colors.background,
-  },
-})
+const useStyle = colors =>
+  StyleSheet.create({
+    root: {
+      backgroundColor: colors.background,
+    },
+  });
